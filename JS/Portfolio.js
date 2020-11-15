79d1aee2586d2f5f47e5d7fa5e22b414ce94a144
Roman Love <66181398+Romeadome@users.noreply.github.com>
2020-11-15 05:34:07 -0500

    portfolio update
    Optimized for ultrawide monitors

--- JS/Portfolio.js
+++ JS/Portfolio.js
@@ -27,9 +27,11 @@ function showSlides(n) {
 	  for (i = 0; i < slides.length; i++) {
 	  slides[i].style.display = "none";  
 	  }
-	  for (i = 0; i < dots.length; i++) {
-		dots[i].className = dots[i].className.replace(" active", "");
-	  }
+    for (const className of dots) {
+      if (className.classList.contains("active")) {
+          className.classList.removeactive("mySlides");
+      }
+  }
   slides[slideIndex-1].style.display = "block";  
   dots[slideIndex-1].className += " active";
 }
