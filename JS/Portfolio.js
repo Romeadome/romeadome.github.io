b02afa9e8d5a66baef7c61a0ba0f0ff3497e3ae7
Roman Love <66181398+Romeadome@users.noreply.github.com>
2020-11-15 15:41:54 -0500

    JS and CSS Fixes
    formatted background to lessen gaps within the columns

--- JS/Portfolio.js
+++ JS/Portfolio.js
@@ -27,11 +27,9 @@ function showSlides(n) {
 	  for (i = 0; i < slides.length; i++) {
 	  slides[i].style.display = "none";  
 	  }
-    for (const className of dots) {
-      if (className.classList.contains("active")) {
-          className.classList.removeactive("mySlides");
+	  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
       }
-  }
   slides[slideIndex-1].style.display = "block";  
   dots[slideIndex-1].className += " active";
 }
